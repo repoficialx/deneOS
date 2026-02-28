9df33cfc0853d14a76f9cd4bc2e880988f93b18f
repoficialx <88939475+repoficialx@users.noreply.github.com>
2026-02-28 09:38:28 +0100

    Add resources

--- deneOS/Properties/Resources.Designer.cs
+++ deneOS/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace deneOS.Properties {
     // a través de una herramienta como ResGen o Visual Studio.
     // Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
     // con la opción /str o recompile su proyecto de VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -546,6 +546,26 @@ namespace deneOS.Properties {
         internal static System.Drawing.Bitmap denelogo {
             get {
                 object obj = ResourceManager.GetObject("denelogo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap deneNaviLogo_NB {
+            get {
+                object obj = ResourceManager.GetObject("deneNaviLogo_NB", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap deneOS_Logo {
+            get {
+                object obj = ResourceManager.GetObject("deneOS_Logo", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -1126,6 +1146,16 @@ namespace deneOS.Properties {
         internal static System.Drawing.Bitmap LaptopPlugInToastImg {
             get {
                 object obj = ResourceManager.GetObject("LaptopPlugInToastImg", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap loading {
+            get {
+                object obj = ResourceManager.GetObject("loading", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
