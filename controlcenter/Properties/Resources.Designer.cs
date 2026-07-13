7e00c63f5e8d1f352f009e289fac10cac64db6c5
repoficialx <88939475+repoficialx@users.noreply.github.com>
2026-07-13 23:54:32 +0200

    add wifi logo

--- controlcenter/Properties/Resources.Designer.cs
+++ controlcenter/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace controlcenter.Properties {
     // a través de una herramienta como ResGen o Visual Studio.
     // Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
     // con la opción /str o recompile su proyecto de VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -166,6 +166,16 @@ namespace controlcenter.Properties {
         internal static System.Drawing.Bitmap icons8_software_100 {
             get {
                 object obj = ResourceManager.GetObject("icons8-software-100", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_wifi_100 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-wifi-100", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
